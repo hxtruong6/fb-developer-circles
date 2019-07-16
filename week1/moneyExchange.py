2f6629db7526d2260698c4a0a81abb69a2b51d3d
Truong Hoang <truong@codelynx.io>
2019-07-16 20:00:43 +0700

    feat: init facebook dev circle course

---
+++ week1/moneyExchange.py
@@ -0,0 +1,15 @@
+bills = [500, 200, 100, 50, 20, 10, 5, 2, 1]
+
+
+def exchange(money, sorted_bills):
+    changes = [0]*len(sorted_bills)
+    due = money
+    for i, bill in enumerate(sorted_bills):
+        if (due > 0):
+            changes[i] = due//bill
+            due -= changes[i]*bill
+    return changes
+
+
+print(exchange(2300, bills))
+print(exchange(54322, bills))
